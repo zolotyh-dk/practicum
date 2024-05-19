6fb93c77bb7ae1176125ffd5580bdb0848d06e5d
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-05-19 11:33:06 +0300

    Решить задачу про параметры строки запроса

---
+++ src/sprint9/uri_query/Practicum.java
@@ -0,0 +1,33 @@
+package sprint9.uri_query;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        // укажите URL запроса, включая его параметры
+        URI url = URI.create("https://functions.yandexcloud.net/d4ed1i6t3f80hf0p7mer?base=RUB&symbols=USD,EUR");
+
+        // создайте объект, описывающий запрос с необходимой информацией
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Код статуса: " + response.statusCode());
+            System.out.println("Ответ: " + response.body());
+        } catch (IOException | InterruptedException e) { // обработка ошибки отправки запроса
+            System.out.println("Во время выполнения запроса ресурса по URL-адресу: '" + url + "' возникла ошибка.\n" +
+                    "Проверьте, пожалуйста, адрес и повторите попытку.");
+        }
+    }
+}
